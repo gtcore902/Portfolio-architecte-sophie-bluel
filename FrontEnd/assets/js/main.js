825d0c1adfa1268ead909020b47d905d59e378c3
gtcore902 <gaetan.tremois@gmail.com>
2023-12-04 15:33:40 +0100

    refactor handle errors and filter's button style

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -15,41 +15,62 @@ function createFigureElement(src, title) {
 }
 
 // Fetch datas from swagger API
+let datas
 async function getDatas() {
     const response = await fetch('http://localhost:5678/api/works')
-    const datas = await response.json()
-    for (let i = 0; i < datas.length; i++) {
-        createFigureElement(datas[i].imageUrl, datas[i].title)
+    // Handle error
+    try {
+        if (response.status === 404) {
+            throw new Error('404, Page not found')
+        }
+        if (response.status === 500) {
+            throw new Error('500, Internal servor error')
+        }
+        datas = await response.json()
+        for (let i = 0; i < datas.length; i++) {
+            createFigureElement(datas[i].imageUrl, datas[i].title)
+        }
+    } catch (error) {
+        console.error(error)
     }
-    for (const element of objectFilterBtn) {
-        element.addEventListener('click', function(event) {
-            if (event.target.dataset.id == 123) {
-                const dataObjects = datas.filter(function(data) {
-                    gallery.innerHTML = ""
-                    return datas
-            })
-                // console.log(dataObjects)
-                for (let i = 0; i < dataObjects.length; i++) {
-                    createFigureElement(dataObjects[i].imageUrl, dataObjects[i].title)
-                }
-            } else {
-                const dataObjects = datas.filter(function(data) {
-                    gallery.innerHTML = ""
-                    return data.categoryId === parseInt(event.target.dataset.id)
-            })
-                // console.log(dataObjects)
-                for (let i = 0; i < dataObjects.length; i++) {
-                    createFigureElement(dataObjects[i].imageUrl, dataObjects[i].title)
-                }
+}
+
+// Launch fetch system
+getDatas()
+
+// Set filter system
+for (const element of objectFilterBtn) {
+    element.addEventListener('click', function(event) {
+        // Get the parent node
+        let elementContainer = document.querySelector('.filters')
+        // Change style of children
+        for (let i = 0; i < elementContainer.children.length; i++) {
+            elementContainer.children[i].style.backgroundColor = 'transparent'
+            elementContainer.children[i].style.color = '#1D6154'
+       }
+       // Change style of current element selected
+        event.target.style.backgroundColor = '#1D6154'
+        event.target.style.color = 'white'
+       // Filter system
+       // Set behavior of the first element
+        if (event.target.dataset.id == 123) {
+            const dataObjects = datas.filter(function(data) {
+                gallery.innerHTML = ""
+                return datas
+             })
+            for (let i = 0; i < dataObjects.length; i++) {
+                createFigureElement(dataObjects[i].imageUrl, dataObjects[i].title)
             }
-        })
-    }
+        } else {
+            // Set behavior of others elements
+            const dataObjects = datas.filter(function(data) {
+                gallery.innerHTML = ""
+                return data.categoryId === parseInt(event.target.dataset.id)
+                })
+            for (let i = 0; i < dataObjects.length; i++) {
+                createFigureElement(dataObjects[i].imageUrl, dataObjects[i].title)
+            }
+        }
+    })
 }
-
-try {
-    getDatas()
-} catch (error) {
-    console.error(error)
-}
-
 
