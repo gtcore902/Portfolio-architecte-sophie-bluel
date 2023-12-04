b0de4037953ced22ca3655b6ee02001c4f13783a
gtcore902 <gaetan.tremois@gmail.com>
2023-12-04 12:13:19 +0100

    feat: implement functional filters

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -1,6 +1,6 @@
 // Variables
 let gallery = document.querySelector('.gallery')
-let objectFilterBtn = document.getElementById('objects')
+let objectFilterBtn = document.querySelectorAll('.filter-btn')
 
 // Creat figure element
 function createFigureElement(src, title) {
@@ -21,16 +21,30 @@ async function getDatas() {
     for (let i = 0; i < datas.length; i++) {
         createFigureElement(datas[i].imageUrl, datas[i].title)
     }
-    objectFilterBtn.addEventListener('click', function(event) {
-        // console.log(event.target)
-        const dataObjects = datas.filter(function(data) {
-            gallery.innerHTML = ""
-            return data.categoryId === 2
-    })
-    for (let i = 0; i < dataObjects.length; i++) {
-        createFigureElement(dataObjects[i].imageUrl, dataObjects[i].title)
+    for (const element of objectFilterBtn) {
+        element.addEventListener('click', function(event) {
+            if (event.target.dataset.id == 123) {
+                const dataObjects = datas.filter(function(data) {
+                    gallery.innerHTML = ""
+                    return datas
+            })
+                // console.log(dataObjects)
+                for (let i = 0; i < dataObjects.length; i++) {
+                    createFigureElement(dataObjects[i].imageUrl, dataObjects[i].title)
+                }
+            } else {
+                const dataObjects = datas.filter(function(data) {
+                    gallery.innerHTML = ""
+                    return data.categoryId === parseInt(event.target.dataset.id)
+            })
+                // console.log(dataObjects)
+                for (let i = 0; i < dataObjects.length; i++) {
+                    createFigureElement(dataObjects[i].imageUrl, dataObjects[i].title)
+                }
+            }
+        })
     }
-})}
+}
 
 try {
     getDatas()
