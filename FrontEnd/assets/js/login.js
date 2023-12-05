3cb945cc7900f552214e8e3a49f83a57d1b4f8a6
gtcore902 <gaetan.tremois@gmail.com>
2023-12-05 10:55:12 +0100

    feat: implemente login function

---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,61 @@
+// Handle form datas
+let inputEmail = document.getElementById('email')
+let inputPassword = document.getElementById('pass')
+let loginForm = document.querySelector('.login-form')
+let errorMessageContainer = document.querySelector('.error-message')
+let errorMessage = 'Email et/ou mot de passe invalide(s)'
+
+
+// Validate email function
+function validateEmail(userMail) {
+    let regexEmail = new RegExp('[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+')
+    return regexEmail.test(userMail)
+}
+
+// Validate password function
+function validatePassword(userPassword) {
+    return userPassword !== ""
+}
+
+// Sending datas function
+async function sendingDatas(datas) {
+    try {
+        const response = await fetch('http://localhost:5678/api/users/login', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: datas
+    },
+    )
+    const result = await response.json()
+    if (!response.ok) {
+        errorMessageContainer.innerText = errorMessage
+        throw new Error("Erreur de traitement fetch")
+    } else {
+        window.localStorage.setItem('token', result.token)
+        console.log(result.status)
+        console.log('Token:' + result.token)
+    }
+    } catch (error) {
+        console.error( error)
+    }
+}
+
+// Validate datas from form and submission
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let userMail = inputEmail.value
+    let userPassword = inputPassword.value
+
+    if (validateEmail(userMail) && validatePassword(userPassword)) {
+        errorMessageContainer.innerText = ""
+        let data = {
+            "email": userMail,
+            "password": userPassword
+        }
+        sendingDatas(JSON.stringify(data))
+    } else {
+        console.log('error datas form')
+        errorMessageContainer.innerText = errorMessage
+    }
+})
+
